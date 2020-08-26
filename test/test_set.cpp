1d4776ab5f922c4cdf9a5e010f5aba0389108302
Leo Ju <armychu1989@gmail.com>
2020-08-26 21:09:42 +0800

    add test_set

--- test/test_set.cpp
+++ test/test_set.cpp
@@ -1,4 +1,5 @@
 #include <set>
+#include <vector>
 #include <iostream>
 #include <limits>
 #include <algorithm>
@@ -6,7 +7,12 @@
 
 int main(int argc, char* argv[])
 {
-    std::set<int, std::greater<int>> set{666, 777, 888};
+    std::set<int, std::less<int>> set{666, 777, 888};
+    std::set<int, std::less<int>> set2{6666, 7777, 8888};
+
+    auto func = set.key_comp();
+    set.swap(set2);
+    std::vector<int> vec {0,1,2,3,4,5,6,7,8,9,10};
     set.emplace(999);
 
     auto res = set.insert(555);
@@ -14,12 +20,25 @@ int main(int argc, char* argv[])
     assert(*res.first == 555);
     assert(res.second);
 
+    set.insert(vec.begin(), vec.end());
+    set.insert({10,11,12,13});
+    set.emplace(14);
+
+    auto end = set.end();
+    std::advance(end, -4);
+    auto iter_erase = set.erase(end);
+    std::cout << "Iterator points to " << *iter_erase << " after erasing." << std::endl;
 
     auto print = [](const int& n) { std::cout << " " << n; };
     auto print_empty_line = [](){std::cout << std::endl;};
 
+    auto range = set.equal_range(7000);
+    std::cout << *range.first << "\t" << 7000 << "\t" << *range.second << std::endl;
+
     std::for_each(set.cbegin(), set.cend(), print);
     print_empty_line();
+
+    std::cout << "There is " << set.count(555) << " \"555\" in set" << std::endl;
 
     set.clear();
     set.empty() ? std::cout << "set is empty." << std::endl
