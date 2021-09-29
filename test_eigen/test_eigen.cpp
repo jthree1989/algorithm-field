4a3daa5751135c5bc0a1a5f1da7f21aefe14b667
Leo Ju <jujianjun@wywk.cn>
2021-09-29 20:08:09 +0800

    add strange behavior while using auto in Eigen

--- test_eigen/test_eigen.cpp
+++ test_eigen/test_eigen.cpp
@@ -16,3 +16,16 @@ TEST_CASE("Matrix", "[Core]"){
                 default_matrix);
   }
 }
+
+TEST_CASE("Matrix multi", "[Core]"){
+  Eigen::Matrix<double, 3, 3> default_matrix;
+  default_matrix << 1, 2, 3, 
+                    4, 5, 6, 
+                    7, 8, 9;
+  auto x = Eigen::Matrix3d::Random();
+  auto y = Eigen::Matrix3d::Random();
+  auto z = x * y;
+  Eigen::Matrix3d z_1 = x * y;
+
+  SPDLOG_INFO("x:\n{}, \ny:\n{}, \nz:\n{}, \nz_1:\n{}", x, y, z, z_1); 
+}
